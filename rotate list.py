c8e28d4f68a8e6c165198b80c563b00c994ccafb
ManognaPunnami-07 <163624228+ManognaPunnami-07@users.noreply.github.com>
2024-03-26 15:23:23 +0530

    Add files via upload

---
+++ rotate list.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k == 0:
+            return head
+        length = 1
+        tail = head
+        while tail.next:
+            length += 1
+            tail = tail.next
+        k = k % length
+        if k == 0:
+            return head
+        newTail = head
+        for i in range(length - k - 1):
+            newTail = newTail.next
+        newHead = newTail.next
+        newTail.next = None
+        tail.next = head
+        return newHead
